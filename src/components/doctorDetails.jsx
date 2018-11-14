1faab4938b33b989e301ef9ee3e6f508fdee028e
abecel22 <abe2211@gmail.com>
2018-11-13 22:06:49 -0600

    Added conditional rendering and setup doctor details component.

--- src/components/doctorDetails.jsx
+++ src/components/doctorDetails.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
 class DoctorDetails extends Component {
+    state = {
+        results: []
+    };
+
+    componentDidMount() {
+        const docId = this.props.match.params.id;
+        const key = process.env.REACT_APP_SECRET_CODE;
+
+        fetch(
+            `https://api.betterdoctor.com/2016-03-01/doctors/${docId}?user_key=${key}`
+        )
+            .then((res) => res.json())
+            .then((result) => {
+                const results = result.data;
+                console.log(results);
+                this.setState({ results: results });
+            });
+    }
+
     render() {
-        return (
-            <div className="container">
-                <h1>Doctor Details</h1>
-            </div>
-        );
+        console.log(this.state.results.profile);
+        const { results } = this.state;
+        if (results.profile) {
+            return (
+                <div className="container">
+                    <h1>Doctor Search Results</h1>
+                    <h3>{results.profile.first_name}</h3>
+                </div>
+            );
+        } else {
+            return (
+                <div className="container">
+                    <h1>Loading....</h1>
+                </div>
+            );
+        }
     }
 }
 
