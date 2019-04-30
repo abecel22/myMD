dc6412ab55cde1aa30f5ce42213879ee073693ce
abecel22 <abe2211@gmail.com>
2019-04-30 05:31:47 -0500

    Improved styling on result cards. Added alt attri.

--- src/components/common/cards.jsx
+++ src/components/common/cards.jsx
@@ -1,89 +1,98 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Cards extends Component {
-    capitalize = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    };
+  capitalize = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
-    formatPhoneNumber = (phoneNumberString) => {
-        var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-        var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-        if (match) {
-            return '(' + match[1] + ') ' + match[2] + '-' + match[3];
-        }
-        return null;
-    };
+  formatPhoneNumber = phoneNumberString => {
+    var cleaned = ("" + phoneNumberString).replace(
+      /\D/g,
+      ""
+    );
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return (
+        "(" + match[1] + ") " + match[2] + "-" + match[3]
+      );
+    }
+    return null;
+  };
 
-    render() {
-        return (
-            <div>
-                {this.props.results.map((result, index) => (
-                    <div className="card mb-3" key={result.uid}>
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-sm-2">
-                                    <img
-                                        src={result.profile.image_url}
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="col-sm-5">
-                                    <h5 className="card-title">{`${
-                                        result.profile.first_name
-                                    } ${result.profile.last_name}`}</h5>
-                                    <h6 className="card-subtitle text-muted">
-                                        {this.capitalize(
-                                            result.specialties[0].uid
-                                        )}
-                                    </h6>
-                                    <p className="pt-3">
-                                        {result.profile.bio.slice(0, 75) +
-                                            '...'}
-                                        <Link to={`/doctorDetails/${index}`}>
-                                            read more
-                                        </Link>
-                                    </p>
-                                </div>
-                                <div className="col-sm-5">
-                                    <ul className="list-group">
-                                        <li className="list-group-item">
-                                            {result.practices[0].name}
-                                        </li>
-                                        <li className="list-group-item">
-                                            {`${
-                                                result.practices[0]
-                                                    .visit_address.street
-                                            }
+  render() {
+    return (
+      <div>
+        {this.props.results.map((result, index) => (
+          <div className="card mb-3" key={result.uid}>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-sm-2">
+                  <img
+                    src={result.profile.image_url}
+                    alt={`Dr. ${
+                      result.profile.last_name
+                    }'s profile`}
+                  />
+                </div>
+                <div className="col-sm-5">
+                  <h5 className="card-title">{`${
+                    result.profile.first_name
+                  } ${result.profile.last_name}`}</h5>
+                  <h6 className="card-subtitle text-muted">
+                    {this.capitalize(
+                      result.specialties[0].uid
+                    )}
+                  </h6>
+                  <p className="pt-3">
+                    {result.profile.bio.slice(0, 75) +
+                      "..."}
+                    <Link to={`/doctorDetails/${index}`}>
+                      read more
+                    </Link>
+                  </p>
+                </div>
+                <div className="col-sm-5">
+                  <ul className="list-group">
+                    <li className="list-group-item">
+                      {result.practices[0].name}
+                    </li>
+                    <li className="list-group-item">
+                      {`${
+                        result.practices[0].visit_address
+                          .street
+                      }
                                             
                                             ${
-                                                result.practices[0]
-                                                    .visit_address.city
+                                              result
+                                                .practices[0]
+                                                .visit_address
+                                                .city
                                             }, ${
-                                                result.practices[0]
-                                                    .visit_address.state_long
-                                            } ${
-                                                result.practices[0]
-                                                    .visit_address.zip
-                                            }`}
-                                        </li>
-                                        <li className="list-group-item">
-                                            Phone:
-                                            {' ' +
-                                                this.formatPhoneNumber(
-                                                    result.practices[0]
-                                                        .phones[0].number
-                                                )}
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                        result.practices[0].visit_address
+                          .state_long
+                      } ${
+                        result.practices[0].visit_address
+                          .zip
+                      }`}
+                    </li>
+                    <li className="list-group-item">
+                      Phone:
+                      {" " +
+                        this.formatPhoneNumber(
+                          result.practices[0].phones[0]
+                            .number
+                        )}
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Cards;
--- src/components/doctorDetails.jsx
+++ src/components/doctorDetails.jsx
@@ -1,88 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class DoctorDetails extends Component {
-    state = {
-        results: [],
-        doctor: {}
-    };
+  state = {
+    results: [],
+    doctor: {}
+  };
 
-    componentDidMount() {
-        //Checks for session storage and adds to state. Reduces call to doc API.
-        const index = this.props.match.params.id;
-        let doctors = sessionStorage.getItem('doctors');
-        doctors = JSON.parse(doctors);
-        const doctor = doctors[index];
-        this.setState({ doctor: doctor });
-        console.log(doctor);
+  componentDidMount() {
+    //Checks for session storage and adds to state. Reduces call to doc API.
+    const index = this.props.match.params.id;
+    let doctors = sessionStorage.getItem("doctors");
+    doctors = JSON.parse(doctors);
+    const doctor = doctors[index];
+    this.setState({ doctor: doctor });
+    console.log(doctor);
+  }
+
+  capitalize = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  render() {
+    console.log(this.state.doctor.profile);
+    const { doctor } = this.state;
+    if (doctor.profile) {
+      return (
+        <div className="detail-container pt-4">
+          <div className="container">
+            <div className="detail-info mt-5">
+              <img
+                src={doctor.profile.image_url}
+                alt={`Dr. ${
+                  doctor.profile.last_name
+                }'s profile`}
+              />
+              <h1 className="mt-3">{`${
+                doctor.profile.first_name
+              } ${doctor.profile.last_name}, ${
+                doctor.profile.title
+              }`}</h1>
+              <h4 className="mb-5">
+                {this.capitalize(doctor.specialties[0].uid)}
+              </h4>
+              <div className="body text-left">
+                <h4>Professional Bio</h4>
+                <p>{doctor.profile.bio}</p>
+                <h4>Practice Name</h4>
+
+                <p>{doctor.practices[0].name}</p>
+                <p>
+                  {doctor.practices[0].visit_address.street}{" "}
+                  <br />
+                  {`${
+                    doctor.practices[0].visit_address.city
+                  }, ${
+                    doctor.practices[0].visit_address.state
+                  } ${
+                    doctor.practices[0].visit_address.zip
+                  }`}
+                </p>
+                <h4>Languages Spoken</h4>
+                <p>
+                  {doctor.profile.languages.map(
+                    (lan, index) => (
+                      <span key={index}>{lan.name}</span>
+                    )
+                  )}
+                </p>
+                <h4>Insurance Accepted</h4>
+                <ul className="list-Columns">
+                  {doctor.insurances.map(
+                    (insurance, index) => (
+                      <li key={index}>
+                        {insurance.insurance_plan.name}
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <h1>Loading....</h1>
+        </div>
+      );
     }
-
-    capitalize = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    };
-
-    render() {
-        console.log(this.state.doctor.profile);
-        const { doctor } = this.state;
-        if (doctor.profile) {
-            return (
-                <div className="detail-container pt-4">
-                    <div className="container">
-                        <div className="detail-info mt-5">
-                            <img src={doctor.profile.image_url} alt="" />
-                            <h1 className="mt-3">{`${
-                                doctor.profile.first_name
-                            } ${doctor.profile.last_name}, ${
-                                doctor.profile.title
-                            }`}</h1>
-                            <h4 className="mb-5">
-                                {this.capitalize(doctor.specialties[0].uid)}
-                            </h4>
-                            <div className="body text-left">
-                                <h4>Professional Bio</h4>
-                                <p>{doctor.profile.bio}</p>
-                                <h4>Practice Name</h4>
-
-                                <p>{doctor.practices[0].name}</p>
-                                <p>
-                                    {doctor.practices[0].visit_address.street}{' '}
-                                    <br />
-                                    {`${
-                                        doctor.practices[0].visit_address.city
-                                    }, ${
-                                        doctor.practices[0].visit_address.state
-                                    } ${doctor.practices[0].visit_address.zip}`}
-                                </p>
-                                <h4>Languages Spoken</h4>
-                                <p>
-                                    {doctor.profile.languages.map(
-                                        (lan, index) => (
-                                            <span key={index}>{lan.name}</span>
-                                        )
-                                    )}
-                                </p>
-                                <h4>Insurance Accepted</h4>
-                                <ul className="list-Columns">
-                                    {doctor.insurances.map(
-                                        (insurance, index) => (
-                                            <li key={index}>
-                                                {insurance.insurance_plan.name}
-                                            </li>
-                                        )
-                                    )}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div className="container">
-                    <h1>Loading....</h1>
-                </div>
-            );
-        }
-    }
+  }
 }
 
 export default DoctorDetails;
